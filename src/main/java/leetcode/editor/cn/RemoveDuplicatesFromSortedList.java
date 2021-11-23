a2206bfc93f62034add476a3d4e20f255ea63946
violetmoon027 <849351538@qq.com>
2021-11-23 14:58:54 +0800

    add 83.删除排序链表中的重复元素

---
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,93 @@
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。 
+//
+// 返回同样按升序排列的结果链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序排列 
+// 
+// Related Topics 链表 👍 678 👎 0
+
+package leetcode.editor.cn;
+
+//Java：删除排序链表中的重复元素
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedList().new Solution();
+        // TO TEST
+        System.out.println(ListNode.parseListNode(solution.deleteDuplicates(ListNode.parseNum(33211))));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode root = head;
+            while (head.next != null) {
+                head = compare(head, head.next);
+            }
+            return root;
+        }
+
+        public ListNode compare(ListNode parent, ListNode current) {
+            if (parent.val == current.val) {
+                parent.next = current.next;
+            } else {
+                parent = current;
+            }
+            return parent;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
+
+//public class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    ListNode() {
+//    }
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
